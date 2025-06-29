f6517e0e838b583aec4238dd8366ffe7e6d9a69c
EmaCrzz <ema_villanueva@hotmail.com>
2025-06-28 21:39:12 -0300

    Actualizar descripción del manifiesto y agregar propiedades de orientación y alcance

--- src/manifest.ts
+++ src/manifest.ts
@@ -4,20 +4,22 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: "Actitud",
     short_name: "Actitud",
-    description: 'PWA para actidud',
+    description: 'PWA para Actitud',
     start_url: '/',
     theme_color: "#ff1168",
     background_color: "#ff1168",
     display: "standalone",
+    orientation: "portrait",
+    scope: "/",
     icons: [
       {
-        src: "/favicon.ico/web-app-manifest-192x192.png",
+        src: "/icon/icon1.png",
         sizes: "192x192",
         type: "image/png",
         purpose: "maskable"
       },
       {
-        src: "/favicon.ico/web-app-manifest-512x512.png",
+        src: "/icon/apple-icon.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable"
