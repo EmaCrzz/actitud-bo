9565340ea38247b95121cf33c7c1e174a963d1c3
EmaCrzz <ema_villanueva@hotmail.com>
2025-09-17 14:38:16 -0300

    internalizacion de auth/*

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 // 🤖 GENERADO AUTOMÁTICAMENTE - NO EDITAR MANUALMENTE
-const CACHE_VERSION = "v1757526318233-9jm5xa"
+const CACHE_VERSION = "v1758123083401-8yfmeh"
 const STATIC_CACHE = `actitud-static-${CACHE_VERSION}`
 const DYNAMIC_CACHE = `actitud-dynamic-${CACHE_VERSION}`
 const RUNTIME_CACHE = `actitud-runtime-${CACHE_VERSION}`
